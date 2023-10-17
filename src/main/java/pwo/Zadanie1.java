ab1fd4d9bf3d212cc4fdb903dee0e3abe332e1fa
Marcin Garbacz <marcingarbacz54@gmail.com>
2023-10-17 17:11:49 +0200

    Wykonanie Zadania 2 - wersja 1

--- src/main/java/pwo/Zadanie1.java
+++ src/main/java/pwo/Zadanie1.java
@@ -9,6 +9,7 @@ public class Zadanie1 {
 
     public static final int FROM = 0, TO = 100;
     public static final String FILE_NAME = "fibseq100.txt";
+    public static final String LUCAS_FILE_NAME = "lucaseq100.txt";
 
     public static void main(String[] args) {
 
@@ -23,5 +24,17 @@ public class Zadanie1 {
         } else {
             System.out.println("Błąd");
         }
+        
+        System.out.println("Ciąg Lucasa");
+
+        boolean lucasOk = SequenceTools.writeToFileLuc(
+                new LucasGenerator(), FROM, TO, LUCAS_FILE_NAME);
+
+        if (lucasOk) {
+            System.out.println("Wynik ciągu Lucasa zapisany do pliku: " + LUCAS_FILE_NAME);
+        } else {
+            System.out.println("Błąd w generowaniu ciągu Lucasa");
+        }
+        
     }
 }
